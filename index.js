bb8010120aa5c47898971a448fee54f8deaf3f81
curran <curran.kelleher@gmail.com>
2015-09-10 16:13:50 -0700

    Implement bidirectional data binding

--- index.js
+++ index.js
@@ -7,34 +7,43 @@
 // between two components:
 //
 //  * Unidirectional  `myComponent.myPropertyA -> myOtherComponent.myPropertyB`
-//  * Bidirectional (planned, not implemented)  `myComponent.myPropertyA <-> myOtherComponent.myPropertyB`
+//  * Bidirectional  `myComponent.myPropertyA <-> myOtherComponent.myPropertyB`
 var ChiasmComponent = require("chiasm-component");
+
 function ChiasmLinks(chiasm) {
 
-  var my = ChiasmComponent({
+  var model = ChiasmComponent({
     bindings: []
   });
 
-  var sourceListeners = [];
+  var listeners = [];
 
-  my.when("bindings", function (bindings){
-
-    var oldSourceListeners = sourceListeners;
-    sourceListeners = [];
+  model.when("bindings", function (bindings){
 
     // Clear out the listeners for the old bindings.
-    oldSourceListeners.forEach(function (_){
-      chiasm.getComponent(_.sourceAlias).then(function (sourceComponent){
-        sourceComponent.cancel(_.listener);
+    // TODO write tests that cover this.
+    listeners.forEach(function (d){
+      chiasm.getComponent(d.alias).then(function (component){
+        components.cancel(d.listener);
       });
     });
+    listeners = [];
 
     // Add listeners for the new bindings.
     bindings.forEach(function(bindingExpr){
 
+      // Determine whether the binding is unidirectional or bidirectional.
+      var bidirectional = false;
+      var operator = "->";
+      if(bindingExpr.indexOf("<->") != -1){
+        bidirectional = true;
+        operator = "<->";
+      }
+
       // Parse the binding expression of the form
-      // "sourceAlias.sourceProperty -> targetAlias.targetProperty"
-      var parts = bindingExpr.split("->").map(function(str){ return str.trim(); }),
+      // "sourceAlias.sourceProperty -> targetAlias.targetProperty" or
+      // "sourceAlias.sourceProperty <-> targetAlias.targetProperty"
+      var parts = bindingExpr.split(operator).map(function(str){ return str.trim(); }),
           source = parts[0].split("."),
           sourceAlias = source[0],
           sourceProperty = source[1],
@@ -45,25 +54,54 @@ function ChiasmLinks(chiasm) {
       // Retreive the source and target components.
       chiasm.getComponent(sourceAlias).then(function(sourceComponent){
         chiasm.getComponent(targetAlias).then(function(targetComponent){
-        
-          // TODO report errors for missing components.
 
-          // Add a reactive function that binds the source to the target.
-          var listener = sourceComponent.when(sourceProperty, function(value){
-            targetComponent[targetProperty] = value;
-          });
+          // TODO write tests that cover this.
+          if(bidirectional){
 
-          // Keep track of the added listener so it can be removed later.
-          sourceListeners.push({
-            sourceAlias: sourceAlias,
-            listener: listener
-          });
+            // Bind source -> target
+            listeners.push({
+              alias: sourceAlias,
+              listener: sourceComponent.when(sourceProperty, function(value){
+                if(!deepEqual(targetComponent[targetProperty], value)){
+                  targetComponent[targetProperty] = value;
+                }
+              })
+            });
+
+            // Bind target -> source
+            listeners.push({
+              alias: targetAlias,
+              listener: targetComponent.when(targetProperty, function(value){
+                if(!deepEqual(sourceComponent[sourceProperty], value)){
+                  sourceComponent[sourceProperty] = value;
+                }
+              })
+            });
+
+          } else {
+
+            // Bind source -> target
+            listeners.push({
+              alias: sourceAlias,
+              listener: sourceComponent.when(sourceProperty, function(value){
+                
+                // No check for equality here, because it could potentially be expensive,
+                // e.g. the common use case of setting a data table via "loader.data -> vis.data".
+                targetComponent[targetProperty] = value;
+              })
+            });
+          }
         });
       });
     });
   });
 
-  return my;
+  // This comparison logic is necessary to avoid an infinite loop in bidirectional data binding.
+  function deepEqual(a, b){
+    return JSON.stringify(a) === JSON.stringify(b);
+  }
+
+  return model;
 }
 
 module.exports = ChiasmLinks;
